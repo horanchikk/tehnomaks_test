65c02c5651ead33fe5c6c4fb10670ab0574f17b1
horanchikk <horanchikk@kansk-tc.ru>
2023-05-02 20:57:31 +0700

    first commit

---
+++ 1/script.js
@@ -0,0 +1,37 @@
+const pageForm = document.querySelector("#pageForm");
+const pageForm__input = document.querySelector(".pageForm__input");
+
+const pageForm__text = document.getElementById("pageForm__text");
+const text__phone = document.getElementById("text__phone");
+
+/**
+ * Checking "+" and "+7" number in input
+ * @param {InputEvent} input
+ */
+function inputted(input) {
+  input.target.value === "+" ? (input.target.value = "+7") : null;
+  input.target.value.slice(0, 2) !== "+7"
+    ? (input.target.value = "+7" + input.target.value)
+    : null;
+}
+
+/**
+ * Sending form
+ * @param {Event} e
+ */
+function sendForm(e) {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  data.set('phone', data.get('phone').substring(1))
+
+  for (let [key, value] of data) {
+    console.log(`${key} - ${value}`);
+  }
+
+  pageForm__text.style.display = 'block';
+  text__phone.textContent = data.get('phone');
+}
+
+pageForm__input.addEventListener("input", inputted);
+pageForm.addEventListener("submit", sendForm);
